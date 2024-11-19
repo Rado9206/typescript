0496ad901f32cdedb897650127b07f2bd8e404ae
Rado <rj.airoo@gmail.com>
2024-11-19 19:00:25 +0100

    todolist first commit

---
+++ 03/src/app.ts
@@ -0,0 +1,7 @@
+/*
+            <li>
+                <label for="task-1">Wyrzucić śmieci</label>
+                <input type="checkbox" id="task-1" name="Wyrzucić śmieci">
+            </li>
+
+			*/
